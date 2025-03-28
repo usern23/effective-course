8808112e4caf280b932b6474ce841544988ca89c
Erlan <erlanchuacbaev@gmail.com>
2025-03-28 15:48:58 +0600

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,21 @@
-import classes from './Footer.module.css'
-import logoMarvel from "../../assets/marvelblacklogo.jpg"
-function Footer(){
-    let CURRENT_YEAR: number=new Date().getFullYear();
-    return(
+import classes from './Footer.module.css';
+import logoMarvel from "../../assets/marvelblacklogo.jpg";
+
+function Footer() {
+    let CURRENT_YEAR: number = new Date().getFullYear();
+    return (
         <footer className={classes.footer}>
             <div className={classes.container}>
                 <div className={classes.content}>
-                    <img src={logoMarvel} className={classes.image}/>
+                    <img src={logoMarvel} className={classes.image} alt="Marvel Logo" />
                     <div className={classes.text}>
-                        <p>Data provided by Marvel. © {CURRENT_YEAR} MARVEL” </p>
-                        <a href="https://developer.marvel.com">developer.marvel.com</a>
+                        <p>Data provided by Marvel. © {CURRENT_YEAR} MARVEL</p>
+                        <a href="https://developer.marvel.com" target="_blank" rel="noopener noreferrer">developer.marvel.com</a>
                     </div>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
+
 export default Footer;
