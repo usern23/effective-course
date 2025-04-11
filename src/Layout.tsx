eff29bf8e396805d39fbcb80e7afed3b22b26f47
Erlan <erlanchuacbaev@gmail.com>
2025-04-11 11:21:51 +0600

    add ToasterContainer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { ToastContainer } from "react-toastify";
 
 function Layout() {
     return (
@@ -10,6 +11,7 @@ function Layout() {
                 <Outlet /> 
             </main>
             <Footer />
+            <ToastContainer/>
         </>
     );
 }
